6a8dba83904fab8eda173a5e0f40db5fc8d44064
Kettle1703 <115614072+Kettle1703@users.noreply.github.com>
2024-09-12 16:44:40 +0500

    Update lib.py

--- lab_1/lib.py
+++ lab_1/lib.py
@@ -1,21 +1,19 @@
 def count_common_elements(*lists):
     if not lists:
         return 0
-
     common_elements = set(lists[0])  # Используем множество для отслеживания общих элементов
-
     for lst in lists[1:]:  # Пересекаем множества с остальными списками
         common_elements.intersection_update(lst)
-
     return len(common_elements)
 
 
-n = int(input('\nВведите количество списков: '))
-plenty = []
-
-for i in range(n):  # Ввод списков
-    user_input = input(f'Введите элементы списка {i + 1}, разделенные пробелами: ')
-    plenty.append(list(map(int, user_input.split())))
-
-result = count_common_elements(*plenty)
-print(f'Количество одинаковых элементов во всех списках: {result}')
+if __name__ == '__main__':
+    n = int(input('\nВведите количество списков: '))
+    plenty = []
+    
+    for i in range(n):  # Ввод списков
+        user_input = input(f'Введите элементы списка {i + 1}, разделенные пробелами: ')
+        plenty.append(list(map(int, user_input.split())))
+    
+    result = count_common_elements(*plenty)
+    print(f'Количество одинаковых элементов во всех списках: {result}')
